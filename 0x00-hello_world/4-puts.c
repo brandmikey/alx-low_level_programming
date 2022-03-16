d1b0c61cd2522dd08180ebdb565b6691c6600d3a
brandmikey <michaelabraham412@>
2022-03-16 16:03:59 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-/*main : prints a phrase 
- * return : returns 0 after printing the phrase
- */
+/*main : prints a phrase and return : returns 0 after printing the phrase*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
