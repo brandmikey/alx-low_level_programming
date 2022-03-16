56dde0dc5e0522cb60af73ccde1c949b388416a6
brandmikey <michaelabraham412@>
2022-03-16 15:55:51 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*This code is to simply print out \"Programming is like building a multilingual puzzle*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
