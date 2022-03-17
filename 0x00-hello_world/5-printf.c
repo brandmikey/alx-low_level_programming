fe8c2d6962e25663c73716cd9d6dacfa13b9ca36
brandmikey <79139229+brandmikey@users.noreply.github.com>
2022-03-17 22:26:22 +0100

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 /* This code is to use the main function to print a sentence */
 #include <stdio.h>
-/* main - a function that prints a sentence */
+/*main - a function that prints a sentence */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
