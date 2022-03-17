c6d9dd4235feb0df829ccff79e7d39a7a0157dd2
brandmikey <79139229+brandmikey@users.noreply.github.com>
2022-03-17 21:28:50 +0100

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
-/**
-  * This code is to print a sentence
-*/
+/* This code is to print a sentence */
 
 #include <stdio.h>
 
-/** 
-  * main : print out phrase
-  * Return : returns 0 after phrase is pronted
-*/
+/* main : print out phrase */
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
+/* return : returns 0 after phrase is pronted */
 return (0);
 }
