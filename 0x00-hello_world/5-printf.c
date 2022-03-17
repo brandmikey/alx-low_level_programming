f12541c29a1ecac71a9b5292f8d71a821dbc12a9
brandmikey <79139229+brandmikey@users.noreply.github.com>
2022-03-17 22:45:10 +0100

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 /* This code is to use the main function to print a sentence */
-#include <stdio.h>
+#include <main.h>
 /**
 * main - a function that uses printf to print a sentence
 * return - returns 0 after phrase is pronted
