e16e0b551859f217c94675deec55e9094bae1239
brandmikey <79139229+brandmikey@users.noreply.github.com>
2022-03-17 16:18:43 +0100

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@
   * main : print out phrase
   * Return : returns 0 after phrase is pronted
 */
-int main (void)
+int main(void)
 {
-         printf("with proper grammar, but the outcome is a piece of art,\n");
-         return(0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return(0);
 
 }
