d592de438f65d8293e2b631d143757a3be69afc4
brandmikey <79139229+brandmikey@users.noreply.github.com>
2022-03-19 20:30:42 +0100

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,7 @@
 * main - a function that uses printf to print a sentence
 * return : (0) void at the end of sentence
 */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
