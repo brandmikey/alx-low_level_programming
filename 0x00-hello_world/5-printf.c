6cc048a8b90b00abbb5ec6ee5af2e775b73e47fc
brandmikey <michaelabraham412@>
2022-03-20 13:42:18 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,");
 /* return : returnws (0) zero at the end of the printing */
 return (0);
 }
