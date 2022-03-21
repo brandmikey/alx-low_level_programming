236b9f14d3c9450fdd76bb11336d962a42629e73
brandmikey <79139229+brandmikey@users.noreply.github.com>
2022-03-21 19:57:44 +0100

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-gcc -c $CFILE
+#include<studio.h>
+/**
+ * main prints the different sizes
+ * return: returned the size 0
+ */
+Int main(void)
+{
+
+        printf("Size of a char: %d byte(s)", sizeof(char))
+        printf("Size of a int: %d byte(s)", sizeof(int))
+        printf("Size of a long int: %d byte(s)", sizeof(long int))
+        printf("Size of a long long int: %d byte(s)", sizeof(long long int))
+        printf("Size of a float: %d byte(s)", sizeof(float))
+
+        return(0);
+}
