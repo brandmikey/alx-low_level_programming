499a33861c7f8426f70dc696f1a6e874f3e723f4
brandmikey <michaelabraham412@>
2022-03-22 08:05:01 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include<stdio>
 /**
- * main: prints sentence
+ * main: writes the sentence
+ *
  * return : 1 after sentence is printed
+ *
+ *
  */
-int main()
+int main(void)
 {
-	fputs("and that piece of art is useful\" \- Dora Korpar\, 2015\-10\-19\n");
+	write("and that piece of art is useful\" \- Dora Korpar\, 2015\-10\-19\n");
 	return (1);
 }
