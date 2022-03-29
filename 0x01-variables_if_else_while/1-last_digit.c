47dfffca0243a8aeec7b50efe52555c2122ee984
brandmikey <michaelabraham412@>
2022-03-29 06:21:04 -0700

    0

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *main - determines value of random variables
+ *and says if it is positive, negative or zero
+ *Return: returns zero
