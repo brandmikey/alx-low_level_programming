f818ad9ff8bf96379bad8873378894373ee4140e
brandmikey <michaelabraham412@>
2022-03-29 09:12:22 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -7,3 +7,28 @@
  *main - determines value of random variables
  *and says if it is positive, negative or zero
  *Return: returns zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+/*your code goes there*/
+	int last;
+
+	last = n % 10;
+	if (last > 5)
+	{
+		printf("%d is greater than 5\n", n);
+	}
+	if (last == 0)
+	{
+		printf("%d is 0\n", n);
+	}
+	else if (last < 6; last != 0)
+	{
+		printf("%d is less than and not 0\n", n);
+	}
+	return (0);
+}
