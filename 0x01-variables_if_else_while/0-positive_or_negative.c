88da192447555ff8c69091746dd12dcdf335bd03
brandmikey <michaelabraham412@>
2022-03-29 04:49:57 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* main: prints files variable n and says if it is positive, negative or zero */
+/* main: prints files variable n 
+ * and says if it is positive, negative or zero */
 
 int main(void)
 {
